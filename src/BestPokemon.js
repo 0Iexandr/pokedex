f749c8480b79f038691efa03844b946cda45ebf4
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2022-08-28 15:43:07 +0300

    Exercise I Done

--- src/BestPokemon.js
+++ src/BestPokemon.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 const BestPokemon = (props) => {
-    console.log(props);
   return (
     <div>
       <p>My favorite Pokemon is Squirtle</p>
       <ul>
-        {props.abilities.map((ability, index) => ( //point of this??
-          <li key={index}>{ability}</li>
+        {props.abilities.map((ability) => (
+          <li key = {ability}>{ability}</li>
         ))}
       </ul>
     </div>
-);
+  );
 };
 
 export default BestPokemon;
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,23 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
-const CaughtPokemon = (props) => {
-  const [caught, setCaught] = useState([]);
-
-  function catchPokemon () {
-    setCaught(caught.concat(["okayy", "okrrr", "okuur", "ok", "k"]));
-
-  }
-
-  console.log(props); //what happens if i am using the array function with no retun, where do I place the variables? A: you can't, it (JSX) doesn't allow for JS code without {}
+function CaughtPokemon(props) {
   return (
-  <>
-    <p>Caught {caught.length} Pokemon on {props.date}</p>
-    <button onClick={catchPokemon}>Click me</button>
-    <ul>{caught.map((el, index) => {
-      return <li key={index}>{el}</li>;
-    })}
-    </ul>
-    </>
-);
-};
+    <p id="p1">Caught 0 Pokemon on {props.date}</p>
+  );
+}
+
 export default CaughtPokemon;
