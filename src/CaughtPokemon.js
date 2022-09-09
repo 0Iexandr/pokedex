fc241ca4381cd165f5aaa1349160c8264f5eceda
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2022-09-09 12:20:48 +0300

    Exercise E Done

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CaughtPokemon(props) {
+  const [caught, setCaught] = useState([]);
+  const pokemons = ["Pikachu", "Bulbasaur", "Ivysaur", "Charmander", "Charizard", "Squirtle", "Wartortle"];
+  const randomPokemon = Math.floor(Math.random() * pokemons.length);
+
+  function catchPokemon() {
+    setCaught(caught.concat(pokemons[randomPokemon]));
+  }
+
   return (
-    <p id="p1">Caught 0 Pokemon on {props.date}</p>
+    <>
+      <p id="p1">Caught {caught.length} Pokemon on {props.date}</p>
+      <button onClick={catchPokemon}>Catch Pokemon</button>
+      <ul>
+      {caught.map((pokemon, idx) => (
+        <li key = {idx}>{pokemon}</li>
+      ))}
+      </ul>
+    </>
   );
 }
 
--- src/Logo.js
+++ src/Logo.js
@@ -4,7 +4,7 @@ const Logo = (props) => {
   return (
     <header>
       <h1>Welcome to the {props.appName}</h1>
-      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" width="300" height="300"/>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" width="300" height="300" onClick={props.handleClick}/>
     </header>
   );
 };
